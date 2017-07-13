75b8eed53ae1331b01bf4136afee8a3807b3578e
michaelrhodes <spam@michaelrhod.es>
2017-07-13 23:06:17 +1000

    Fix support check and method usage

--- browser.js
+++ browser.js
@@ -1,14 +1,15 @@
 module.exports = (function (global) {
   var uint32 = 'Uint32Array' in global
   var crypto = global.crypto || global.msCrypto
-  var rando = typeof crypto.getRandomValues === 'function'
+  var rando = crypto && typeof crypto.getRandomValues === 'function'
   var good = uint32 && crypto && rando
   if (!good) return Math.random
 
   var arr = new Uint32Array(1)
   var max = Math.pow(2, 32)
   function random () {
-    return crypto.getRandomValues(arr)[0] / max
+    crypto.getRandomValues(arr)
+    return arr[0] / max
   }
 
   random.cryptographic = true
